07aa9c3318797894b6ac02793a07c6786237acd1
Tom McCann <tommccann777@gmail.com>
2025-04-07 15:31:35 +1200

    Changed compile script for compiling beta

--- compile_beta.sh
+++ compile_beta.sh
@@ -1,1 +1,2 @@
-eosio-cpp -o freeosgov.wasm freeosgov.cpp -DBETA --abigen
+eosio-cpp -I /usr/local/Cellar/eosio.cdt/1.8.1/opt/eosio.cdt/include/eosiolib/contracts -o freeosgov.wasm freeosgov.cpp -DBETA --abigen
+
